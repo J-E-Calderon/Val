55e663477c334f012dd03893d8524815fc75a17b
J-E-Calderon <juan.eduardo.calderon@correounivalle.edu.co>
2025-02-08 15:50:19 -0500

    text

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-const buttonNo = document.querySelector('#no');
-const buttonYes = document.querySelector('#yes');
-const buttonWhySure = document.querySelector('#whySure');
+const buttonNo = document.querySelector('#no')
+const buttonYes = document.querySelector('#yes')
 
-let fontSize = 2;
+let fontSize = 2
 
 let messages = [
   'Sisan´t?',
@@ -13,21 +12,17 @@ let messages = [
   'Si viste el otro boton?',
   'Luego que porque eres una chucha',
   'Ya mejor dime que no me quieres ;( '
-];
+]
 
 buttonNo.addEventListener('click', () => {
-  fontSize = fontSize + .5;
-  buttonYes.style.fontSize = `${fontSize}rem`;
+  fontSize = fontSize + .5
+  buttonYes.style.fontSize = `${fontSize}rem`
 
-  const indexRandom = Math.floor(Math.random() * messages.length);
-  buttonNo.textContent = messages[indexRandom];
-});
+  const indexRandom = Math.floor(Math.random() * messages.length)
+
+  buttonNo.textContent = messages[indexRandom]
+})
 
 buttonYes.addEventListener('click', () => {
-  document.querySelector('#message').style.display = 'flex';
-  buttonWhySure.style.display = 'block';
-});
-
-buttonWhySure.addEventListener('click', () => {
-  window.location.href = 'leona.html';
-});
+  document.querySelector('#message').style.display = 'flex'
+})
